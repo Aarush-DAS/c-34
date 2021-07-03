589c756c6aa06b367534626844e2091d5a8a06da
Aarush-DAS <78961766+Aarush-DAS@users.noreply.github.com>
2021-07-03 18:10:43 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+const Engine=Matter.Engine;
+const World=Matter.World;
+const Bodies=Matter.Bodies;
+const Constraint=Matter.Constraint;
+
+var engine,world;
+var box1=[];
+var ball;
+
+function setup() {
+  createCanvas(1200,800);
+  engine=Engine.create();
+  world=engine.world;
+ 
+  ground=new Ground(600,750,1200,20);
+  for(var i=0;i<=700;i=i+100){
+    box1.push(new Box(700,i,70,70));
+  }
+
+  ball= new Ball(200,200,80,80);
+  rope=new Rope(ball.body,{x:500,y:50 })
+}
+
+function draw() {
+  background(0);  
+  Engine.update(engine);
+  ground.display();
+  for(var i=0; i<box1.length;i++){
+    box1[i].display();
+    }
+    ball.display();
+    rope.display();
+}
+
+function mouseDragged() {
+  Matter.Body.setPosition(ball.body, { x: mouseX, y: mouseY });
+}
